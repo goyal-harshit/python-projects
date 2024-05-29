27453af95a7c3f38cc4cf6a7cdccbcca0471b70a
Harshit Goyal <harshitgoyal780p@gmail.com>
2024-05-30 04:06:48 +0530

    Changes done

--- Pong Game/ball.py
+++ Pong Game/ball.py
@@ -5,8 +5,8 @@ class Ball(Turtle):
 	def __init__(self):
 		super().__init__()
 		self.shape("circle")
-		self.penup()
 		self.color("white")
+		self.penup()
 		self.x_move = 10
 		self.y_move = 10
 
