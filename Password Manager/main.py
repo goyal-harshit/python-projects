77954cc87c13cb3b7e3179f25c3d1f92dcc546dd
Harshit Goyal <harshitgoyal780p@gmail.com>
2024-06-24 15:01:18 +0530

    Changes done

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,5 +1,54 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from tkinter import *
+
+
+# ---------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+	pass
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+	with open("data.txt", "a") as file:
+		website = website_entry.get()
+		mail = mail_entry.get()
+		password = pass_entry.get()
+		file.write(f"{website} | {mail} | {password}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=2, row=1)
+
+website_label = Label(text="Website:")
+website_label.grid(column=1, row=2)
+
+website_entry = Entry(width=53)
+website_entry.grid(column=2, row=2, columnspan=2)
+website_entry.focus()
+
+mail_label = Label(text="Email/Username:")
+mail_label.grid(column=1, row=3)
+
+mail_entry = Entry(width=53)
+mail_entry.grid(column=2, row=3, columnspan=2)
+mail_entry.insert(index=END, string="harshitgoyal780p@gmail.com")
+
+pass_label = Label(text="Password:")
+pass_label.grid(column=1, row=4)
+
+pass_entry = Entry(width=34)
+pass_entry.grid(column=2, row=4)
+
+pass_generate_button = Button(text="Generate Password", command=generate_password)
+pass_generate_button.grid(column=3, row=4)
+
+add_button = Button(text="Add", width=45, command=save_password)
+add_button.grid(column=2, row=5, columnspan=2)
+
+window.mainloop()
