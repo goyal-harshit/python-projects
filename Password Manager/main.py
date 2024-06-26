9d03e4ad1f2a6c69c2fa120068c432e4640e0f1d
Harshit Goyal <harshitgoyal780p@gmail.com>
2024-06-26 18:10:31 +0530

    Changes done

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,22 +1,60 @@
 from tkinter import *
+from tkinter import messagebox
+import pyperclip
+import random
 
 
 # ---------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-	pass
+
+	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+			   'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+			   'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+	nr_letters = random.randint(6, 8)
+	nr_symbols = random.randint(2, 4)
+	nr_numbers = random.randint(2, 4)
+
+	password_list = []
+
+	for char in range(nr_letters):
+		password_list.append(random.choice(letters))
+
+	for char in range(nr_symbols):
+		password_list += random.choice(symbols)
+
+	for char in range(nr_numbers):
+		password_list += random.choice(numbers)
+
+	random.shuffle(password_list)
+
+	password = "".join(password_list)
+
+	pyperclip.copy(password)
+
+	pass_entry.delete(0, END)
+	pass_entry.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-
 	website = website_entry.get()
 	mail = mail_entry.get()
 	password = pass_entry.get()
 
-	with open("passwords.txt", "a") as file:
-		file.write(f"{website} | {mail} | {password}\n")
-		website_entry.delete(0, END)
-		pass_entry.delete(0, END)
+	if len(website) == 0 or len(password) == 0 or len(mail) == 0:
+		messagebox.showinfo(title="Missing Info", message="Please make sure you haven't left any fields empty")
+	else:
+		is_ok = messagebox.askyesno(title=website,
+									message=f"These are the details entered: \nEmail: {mail} \nPassword: {password}\nIs the information correct?")
+		if is_ok:
+			with open("passwords.txt", "a") as file:
+				file.write(f"{website} | {mail} | {password}\n")
+				website_entry.delete(0, END)
+				pass_entry.delete(0, END)
+				website_entry.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
